d627cdf84f71cb898dbaa258b4d96af163db3ce1
MurrayAK <murraykeele@gmail.com>
2014-01-15 02:57:55 +0000

    update

--- boot/main.cpp
+++ boot/main.cpp
@@ -1,6 +1,5 @@
 // *****************************************************************************************
 // ** main.cpp
-// ** written by Murray Keele
 // *****************************************************************************************
 
 #include <string>
@@ -49,9 +48,9 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
 *                default of nullptr draws the entire texture
 */
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
-        SDL_Rect *clip = nullptr)
+    SDL_Rect *clip = nullptr)
 {
-        SDL_RenderCopy(ren, tex, clip, &dst);
+    SDL_RenderCopy(ren, tex, clip, &dst);
 }
 
 /**
@@ -67,19 +66,19 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
 *                default of nullptr draws the entire texture
 */
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
-        SDL_Rect *clip = nullptr)
+    SDL_Rect *clip = nullptr)
 {
-        SDL_Rect dst;
-        dst.x = x;
-        dst.y = y;
-        if (clip != nullptr) {
-                dst.w = clip->w;
-                dst.h = clip->h;
-        }
-        else
-                SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+    SDL_Rect dst;
+    dst.x = x;
+    dst.y = y;
+    if (clip != nullptr) {
+        dst.w = clip->w;
+        dst.h = clip->h;
+    }
+    else
+        SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
 
-        renderTexture(tex, ren, dst, clip);
+    renderTexture(tex, ren, dst, clip);
 }
 
 /**
@@ -92,30 +91,30 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
 * @return An SDL_Texture containing the rendered message, or nullptr if something went wrong
 */
 SDL_Texture* renderText(const std::string &message, const std::string &fontFile,
-        SDL_Color color, int fontSize, SDL_Renderer *renderer)
+    SDL_Color color, int fontSize, SDL_Renderer *renderer)
 {
-        //Open the font
-        TTF_Font *font = TTF_OpenFont(fontFile.c_str(), fontSize);
-        if (font == nullptr){
-                logSDLError(std::cout, "TTF_OpenFont");
-                return nullptr;
-        }        
-        //We need to first render to a surface as that's what TTF_RenderText
-        //returns, then load that surface into a texture
-        SDL_Surface *surf = TTF_RenderText_Blended(font, message.c_str(), color);
-        if (surf == nullptr){
-                TTF_CloseFont(font);
-                logSDLError(std::cout, "TTF_RenderText");
-                return nullptr;
-        }
-        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
-        if (texture == nullptr){
-                logSDLError(std::cout, "CreateTexture");
-        }
-        //Clean up the surface and font
-        SDL_FreeSurface(surf);
+    //Open the font
+    TTF_Font *font = TTF_OpenFont(fontFile.c_str(), fontSize);
+    if (font == nullptr){
+        logSDLError(std::cout, "TTF_OpenFont");
+        return nullptr;
+    }        
+    //We need to first render to a surface as that's what TTF_RenderText
+    //returns, then load that surface into a texture
+    SDL_Surface *surf = TTF_RenderText_Blended(font, message.c_str(), color);
+    if (surf == nullptr) {
         TTF_CloseFont(font);
-        return texture;
+        logSDLError(std::cout, "TTF_RenderText");
+        return nullptr;
+    }
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
+    if (texture == nullptr) {
+        logSDLError(std::cout, "CreateTexture");
+    }
+    //Clean up the surface and font
+    SDL_FreeSurface(surf);
+    TTF_CloseFont(font);
+    return texture;
 }
 
 int engineInit() {
@@ -125,19 +124,19 @@ int engineInit() {
         return 1;
     }
 
-        //Start up SDL-IMAGE and make sure it went ok
-        if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
-                logSDLError(std::cout, "IMG_Init");
-                return 1;
-        }
+    //Start up SDL-IMAGE and make sure it went ok
+    if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
+        logSDLError(std::cout, "IMG_Init");
+		return 1;
+    }
 
-        //Start up SDL-TTF and make sure it went ok
-        if (TTF_Init() != 0) {
-                logSDLError(std::cout, "TTF_Init");
-                return 1;
-        }
+    //Start up SDL-TTF and make sure it went ok
+    if (TTF_Init() != 0) {
+        logSDLError(std::cout, "TTF_Init");
+        return 1;
+    }
 
-        return 0;
+    return 0;
 }
 
 int main(int argc, char** argv)
@@ -146,9 +145,9 @@ int main(int argc, char** argv)
 
     //Setup our window and renderer
     SDL_Window *window = SDL_CreateWindow("boot", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == nullptr) { logSDLError(std::cout, "CreateWindow"); return 2; }
+    if (window == nullptr) { logSDLError(std::cout, "CreateWindow"); return 2; }
 
-        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr) { logSDLError(std::cout, "CreateRenderer"); return 3; }
         
     SDL_Event e; //Our event structure
@@ -161,64 +160,64 @@ int main(int argc, char** argv)
         {
         //Read any events that occured
         while (SDL_PollEvent(&e))
-                {
+        {
             if (e.type == SDL_QUIT) quit = true;
 
-                        //Keyboard events
-                        if (e.type == SDL_KEYDOWN) {
-                                switch (e.key.keysym.sym) {
-                                        case SDLK_ESCAPE:
-                                                quit = true;
-                                                break;
-                                        default:
-                                                break;
-                                }
-                        }
+            //Keyboard events
+            if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
 
-                        //Mouse button events
-                        if (e.type == SDL_MOUSEBUTTONDOWN) {
-                                switch (e.button.button) {
-                                        case SDL_BUTTON_LEFT:
-                                                newvec[0] = e.button.x;
-                                                newvec[1] = e.button.y;
-                                                drawPoints.push_back(newvec);
-                                                break;
-                                        case SDL_BUTTON_RIGHT:
-                                                drawPoints.clear();
-                                                break;
-                                        default:
-                                                break;
-                                }
-                        }
+            //Mouse button events
+            if (e.type == SDL_MOUSEBUTTONDOWN) {
+                switch (e.button.button) {
+                    case SDL_BUTTON_LEFT:
+                        newvec[0] = e.button.x;
+                        newvec[1] = e.button.y;
+                        drawPoints.push_back(newvec);
+                        break;
+                    case SDL_BUTTON_RIGHT:
+                        drawPoints.clear();
+                        break;
+                    default:
+                            break;
+                }
+            }
 
-                        //Mouse motion events
-                        if (e.type == SDL_MOUSEMOTION) {
-                                mouseVec[0] = e.motion.x;
-                                mouseVec[1] = e.motion.y;
-                        }
+            //Mouse motion events
+            if (e.type == SDL_MOUSEMOTION) {
+                mouseVec[0] = e.motion.x;
+                mouseVec[1] = e.motion.y;
+            }
         }
 
         //Rendering
         SDL_RenderClear(renderer);
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
                 
-                //Draw Points
-                std::vector< std::vector<int> >::iterator r;
-                std::vector<int>::iterator c;
-                std::vector<int> vector(2);
-                for (r = drawPoints.begin(); r != drawPoints.end(); r++) {
-                        vector.clear();
-                        for (c = r->begin(); c != r->end(); c++) vector.push_back(*c);
-                        SDL_RenderDrawPoint(renderer, vector[0], vector[1]);
-                        //std::cout << 'x' << vector[0] << ', y' << vector[1] << '\n';
-                }
+        //Draw Points
+        std::vector< std::vector<int> >::iterator r;
+        std::vector<int>::iterator c;
+        std::vector<int> vector(2);
+        for (r = drawPoints.begin(); r != drawPoints.end(); r++) {
+            vector.clear();
+            for (c = r->begin(); c != r->end(); c++) vector.push_back(*c);
+            SDL_RenderDrawPoint(renderer, vector[0], vector[1]);
+            //std::cout << 'x' << vector[0] << ', y' << vector[1] << '\n';
+        }
 
-                //Draw mouse target visualizer
-                SDL_SetRenderDrawColor(renderer, 108, 158, 249, 255);
-                SDL_RenderDrawLine(renderer, mouseVec[0], 0, mouseVec[0], SCREEN_HEIGHT);
-                SDL_RenderDrawLine(renderer, 0, mouseVec[1], SCREEN_WIDTH, mouseVec[1]);
+        //Draw mouse target visualizer
+        SDL_SetRenderDrawColor(renderer, 108, 158, 249, 255);
+        SDL_RenderDrawLine(renderer, mouseVec[0], 0, mouseVec[0], SCREEN_HEIGHT);
+        SDL_RenderDrawLine(renderer, 0, mouseVec[1], SCREEN_WIDTH, mouseVec[1]);
 
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0); //Set screen back to black
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0); //Set screen back to black
         SDL_RenderPresent(renderer); //Update the screen
     }
 
@@ -226,7 +225,7 @@ int main(int argc, char** argv)
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
-        TTF_Quit();
+    TTF_Quit();
     IMG_Quit();
     SDL_Quit();
         
